71a7715e7bdf143c6851d07ba19cb8d581f494c6
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-02-16 13:03:10 -0800

    Updated urls.py and views.py to a Class Based Code.

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products_list),
-    path('<int:pk>/', views.product_detail),
+    path('', views.Product_List.as_view()),
+    path('<int:pk>/', views.Product_Detail.as_view()),
 ]
